0d5978961122b814b07c6e71bc7e1412497e5ef2
Hans Pinckaers <hans.pinckaers@gmail.com>
2013-10-08 18:20:00 +0100

    Initial project.

---
+++ Alertify/HPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HPAppDelegate.h
+//  Alertify
+//
+//  Created by Hans Pinckaers on 08-10-13.
+//  Copyright (c) 2013 Hans Pinckaers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
