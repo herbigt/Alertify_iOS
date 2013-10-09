8ea21a813ed4cfb58a3e4c4ae8790e0234cabe0a
Hans Pinckaers <hans.pinckaers@gmail.com>
2013-10-09 20:17:44 +0100

    NOT memory efficient, but I like the effect better.

--- Alertify/ViewControllers/HPAlarmListFlowLayout.m
+++ Alertify/ViewControllers/HPAlarmListFlowLayout.m
@@ -17,9 +17,9 @@
     if ((self = [super init]))
     {
         self.minimumInteritemSpacing = 10;
-        self.minimumLineSpacing = 1;
+        self.minimumLineSpacing = 0;
         self.itemSize = CGSizeMake(320, 64);
-        self.sectionInset = UIEdgeInsetsZero;
+        self.sectionInset = UIEdgeInsetsMake(0, 0, 64, 0);
         
         self.dynamicAnimator = [[UIDynamicAnimator alloc] initWithCollectionViewLayout:self];
         self.visibleIndexPathsSet = [NSMutableSet set];
@@ -68,8 +68,6 @@
         case UIGestureRecognizerStateBegan:
         case UIGestureRecognizerStateChanged: {
             
-            NSLog(@"chanced! %@", gestureRecognizer);
-            
         } break;
         case UIGestureRecognizerStateCancelled:
         case UIGestureRecognizerStateEnded: {
@@ -86,60 +84,24 @@
     [super prepareLayout];
     
     // Need to overflow our actual visible rect slightly to avoid flickering.
-    CGRect visibleRect = CGRectInset((CGRect){.origin = self.collectionView.bounds.origin, .size = self.collectionView.frame.size}, -100, -100);
+//    CGRect visibleRect = CGRectInset((CGRect){.origin = self.collectionView.bounds.origin, .size = self.collectionView.frame.size}, -100, -100);
+    CGSize contentSize = self.collectionView.contentSize;
+    NSArray *items = [super layoutAttributesForElementsInRect:
+                      CGRectMake(0.0f, 0.0f, contentSize.width, contentSize.height)];
     
-    NSArray *itemsInVisibleRectArray = [super layoutAttributesForElementsInRect:visibleRect];
-    
-    NSSet *itemsIndexPathsInVisibleRectSet = [NSSet setWithArray:[itemsInVisibleRectArray valueForKey:@"indexPath"]];
-    
-    // Step 1: Remove any behaviours that are no longer visible.
-    NSArray *noLongerVisibleBehaviours = [self.dynamicAnimator.behaviors filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(UIAttachmentBehavior *behaviour, NSDictionary *bindings) {
-        BOOL currentlyVisible = [itemsIndexPathsInVisibleRectSet member:[[[behaviour items] firstObject] indexPath]] != nil;
-        return !currentlyVisible;
-    }]];
-    
-    [noLongerVisibleBehaviours enumerateObjectsUsingBlock:^(id obj, NSUInteger index, BOOL *stop) {
-        [self.dynamicAnimator removeBehavior:obj];
-        [self.visibleIndexPathsSet removeObject:[[[obj items] firstObject] indexPath]];
-    }];
-    
-    // Step 2: Add any newly visible behaviours.
-    // A "newly visible" item is one that is in the itemsInVisibleRect(Set|Array) but not in the visibleIndexPathsSet
-    NSArray *newlyVisibleItems = [itemsInVisibleRectArray filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(UICollectionViewLayoutAttributes *item, NSDictionary *bindings) {
-        BOOL currentlyVisible = [self.visibleIndexPathsSet member:item.indexPath] != nil;
-        return !currentlyVisible;
-    }]];
-    
-    
-    CGPoint tTouchLocation = [self.collectionView.panGestureRecognizer locationInView:self.collectionView];
-    if(tTouchLocation.y != 0) touchLocation = tTouchLocation;
-    
-    [newlyVisibleItems enumerateObjectsUsingBlock:^(UICollectionViewLayoutAttributes *item, NSUInteger idx, BOOL *stop) {
-        CGPoint center = item.center;
-        UIAttachmentBehavior *springBehaviour = [[UIAttachmentBehavior alloc] initWithItem:item attachedToAnchor:center];
-        
-        springBehaviour.length = 0.0f;
-        springBehaviour.damping = 0.8f;
-        springBehaviour.frequency = 1.0f;
-        
-        // If our touchLocation is not (0,0), we'll need to adjust our item's center "in flight"
-        if (!CGPointEqualToPoint(CGPointZero, touchLocation)) {
-            CGFloat yDistanceFromTouch = fabsf(touchLocation.y - springBehaviour.anchorPoint.y);
-            CGFloat xDistanceFromTouch = fabsf(touchLocation.x - springBehaviour.anchorPoint.x);
-            CGFloat scrollResistance = (yDistanceFromTouch + xDistanceFromTouch) / 500.0f;
+    if (self.dynamicAnimator.behaviors.count == 0) {
+        [items enumerateObjectsUsingBlock:^(id<UIDynamicItem> obj, NSUInteger idx, BOOL *stop) {
+            UIAttachmentBehavior *behaviour = [[UIAttachmentBehavior alloc] initWithItem:obj
+                                                                        attachedToAnchor:[obj center]];
             
-            if (self.latestDelta < 0) {
-                center.y += MAX(self.latestDelta, self.latestDelta*scrollResistance);
-            }
-            else {
-                center.y += MIN(self.latestDelta, self.latestDelta*scrollResistance);
-            }
-            item.center = center;
-        }
-        
-        [self.dynamicAnimator addBehavior:springBehaviour];
-        [self.visibleIndexPathsSet addObject:item.indexPath];
-    }];
+            behaviour.length = 0.0f;
+            behaviour.damping = 0.9f;
+            behaviour.frequency = 1.0f;
+            
+            [self.dynamicAnimator addBehavior:behaviour];
+        }];
+    }
+
 }
 
 -(NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
@@ -157,25 +119,30 @@
     UIScrollView *scrollView = self.collectionView;
     CGFloat delta = newBounds.origin.y - scrollView.bounds.origin.y;
     self.latestDelta = delta;
-
+    
     CGPoint tTouchLocation = [self.collectionView.panGestureRecognizer locationInView:self.collectionView];
     if(tTouchLocation.y != 0) touchLocation = tTouchLocation;
     
     [self.dynamicAnimator.behaviors enumerateObjectsUsingBlock:^(UIAttachmentBehavior *springBehaviour, NSUInteger idx, BOOL *stop) {
-        CGFloat yDistanceFromTouch = fabsf(touchLocation.y - springBehaviour.anchorPoint.y);
-        CGFloat xDistanceFromTouch = fabsf(touchLocation.x - springBehaviour.anchorPoint.x);
-        CGFloat scrollResistance = (yDistanceFromTouch + xDistanceFromTouch) / 500.0f;
-        
         UICollectionViewLayoutAttributes *item = springBehaviour.items.firstObject;
-        CGPoint center = item.center;
-        if (delta < 0) {
-            center.y += MAX(delta, delta*scrollResistance);
+
+        if([springBehaviour isKindOfClass:[UIAttachmentBehavior class]])
+        {
+            CGFloat yDistanceFromTouch = fabsf(touchLocation.y - springBehaviour.anchorPoint.y);
+            CGFloat xDistanceFromTouch = fabsf(touchLocation.x - springBehaviour.anchorPoint.x);
+            CGFloat scrollResistance = (yDistanceFromTouch + xDistanceFromTouch) / 750.0f;
+            
+            CGPoint center = item.center;
+            if (delta < 0) {
+                center.y += self.latestDelta*scrollResistance;
+            }
+            else {
+                center.y += self.latestDelta*scrollResistance;
+            }
+
+            item.center = center;
         }
-        else {
-            center.y += MIN(delta, delta*scrollResistance);
-        }
-        item.center = center;
-        
+
         [self.dynamicAnimator updateItemUsingCurrentState:item];
     }];
     
